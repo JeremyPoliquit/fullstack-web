d32b19e35c226f4daabe0df40a23901a8a749a5d
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-23 03:08:17 -0800

    Update is working now by all usser and by selected user

--- backend/src/main/java/com/spring/backend/Service/UserService.java
+++ backend/src/main/java/com/spring/backend/Service/UserService.java
@@ -1,6 +1,8 @@
 package com.spring.backend.Service;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,20 +16,51 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
     // Create User
     public User createUser(User user) {
         return userRepository.save(user);
     }
 
-    public User updatedUser(long id, User userUpdate) {
-        User user = userRepository.findById(id);
-        user.setUsername(user.getUsername());
-        user.setPassword(user.getPassword());
-        return userRepository.save(user);
+    // Retrieve User
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Update User
+    public User updateUser(long id, User updateUser) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setUsername(updateUser.getUsername());
+            user.setPassword(updateUser.getPassword());
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("User with ID " + id + " not found");
+        }
+    }
+
+    // Update only Username
+    public User updateUsername(long id, User updateUsername) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User username = optionalUser.get();
+            username.setUsername(updateUsername.getUsername());
+            return userRepository.save(username);
+        } else {
+            throw new RuntimeException("User with ID " + id + " not found");
+        }
+    }
+
+    // Update only Password
+    public User updatePassword(long id, User updatePassword) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User password = optionalUser.get();
+            password.setPassword(updatePassword.getPassword());
+            return userRepository.save(password);
+        } else {
+            throw new RuntimeException("User with ID " + id + " not found");
+        }
     }
 
 }
